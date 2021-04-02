1fa5851f1168c5d614f34a90ea9138a3efcc52ba
khanr <khanriza@gmail.com>
2021-04-02 19:32:04 -0400

    removed some extra stuff

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,66 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="navbar-brand" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="navbar-brand" to="/contact"
-              >Contact</router-link
-            >
-          </li>
-          <li class="nav-item">
-            <router-link class="navbar-brand" to="/about">About</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="navbar-brand" to="/user/123">User</router-link>
-          </li>
-          <li class="nav-item">
-            <button @click.prevent="logout">Logout</button>
-          </li>
-        </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="navbar-brand" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="navbar-brand" to="/contact"
+                >Contact</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <router-link class="navbar-brand" to="/about">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="navbar-brand" to="/user/123"
+                >User</router-link
+              >
+            </li>
+            <li class="nav-item">
+              <button @click.prevent="logout">Logout</button>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
-  <router-view />
+    </nav>
+    <router-view />
+    <Snackbar />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
 import firebase from "firebase";
+import Snackbar from "@/components/Snackbar.vue";
 
 export default defineComponent({
   name: "App",
+  components: { Snackbar },
   setup() {
-    async function logout() {
-      try {
-        await firebase.auth().signOut();
-      } catch (e) {
-        console.log(e);
-      }
+    const router = useRouter();
+
+    function logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => router.push("/auth/login"));
     }
 
     return { logout };
@@ -60,6 +69,13 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+*,
+*:before,
+*:after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Snackbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="snackbar">
+    <h4>Title</h4>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam,
+      culpa.
+    </p>
+    <button>Close</button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Snackbar",
+});
+</script>
+
+<style lang="scss" scoped>
+.snackbar {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  padding: 1rem;
+
+  h4,
+  p {
+    text-align: left;
+  }
+
+  button {
+    width: 150px;
+  }
+}
+</style>
